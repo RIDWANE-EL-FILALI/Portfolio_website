11dd49493de346ae9f270f1dcbff9f69a536b5e8
RIDWANE-EL-FILALI <ridwaneelfilali@gmail.com>
2024-06-11 07:40:39 +0100

    done

---
+++ src/utils/dark_mode.js
@@ -0,0 +1,33 @@
+export default function setTheme() {
+    const themeToggleButtons = document.querySelectorAll('#theme-toggle');
+    const themeImages = document.querySelectorAll('.theme-img');
+    //storage api used to store the theme in the local storage
+    let theme = localStorage.getItem('theme') || 'dark';
+
+    if (theme === 'light') {
+        document.body.classList.add('light_mode');
+        themeImages.forEach(image => {
+            image.src = image.src.replace('Light', 'Dark');
+        });
+    }
+
+    themeToggleButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            if (theme === 'dark') {
+                theme = 'light';
+                document.body.classList.add('light_mode');
+                themeImages.forEach(image => {
+                    image.src = image.src.replace('Light', 'Dark');
+                });
+            } else {
+                theme = 'dark';
+                document.body.classList.remove('light_mode');
+                themeImages.forEach(image => {
+                    image.src = image.src.replace('Dark', 'Light');
+                });
+            }
+            localStorage.setItem('theme', theme);
+        });
+    });
+}
+
---
+++ src/utils/mobile-nav.js
@@ -0,0 +1,26 @@
+export default function mobileNav() {
+    const button = document.querySelector('.header__bars');
+    const nav = document.querySelector('.mobile-nav');
+    const mobileNavLinks = document.querySelectorAll('.mobile-nav__link');
+
+    let isMobileNavOpen = false;
+
+    mobileNavLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            document.body.style.overflowY = 'auto';
+            nav.style.display = 'none';
+            isMobileNavOpen = false;
+        });
+    });
+
+    button.addEventListener('click', () => {
+        if (isMobileNavOpen) {
+            nav.style.display = 'none';
+            isMobileNavOpen = false;
+            document.body.style.overflowY = 'auto';
+        } else {
+            nav.style.display = 'flex';
+            isMobileNavOpen = true;
+        }
+    });
+}
