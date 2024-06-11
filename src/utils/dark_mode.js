fd137f31c7c1c8790659ef4d2ad3735aa7c1b1e7
RIDWANE-EL-FILALI <ridwaneelfilali@gmail.com>
2024-06-11 08:40:21 +0100

    fixed issue in index.html

--- src/utils/dark_mode.js
+++ src/utils/dark_mode.js
@@ -1,34 +1,33 @@
 export default function setTheme() {
     const themeToggleButtons = document.querySelectorAll('#theme-toggle');
     const themeImages = document.querySelectorAll('.theme-img');
-    //storage api used to store the theme in the local storage
+    // Retrieve theme from local storage or default to 'dark'
     let theme = localStorage.getItem('theme') || 'dark';
 
-    if (theme === 'light') {
-        document.body.classList.add('light_mode');
+    // Function to toggle theme
+    function toggleTheme() {
         themeImages.forEach(image => {
-            image.src = image.src.replace('Light', 'Dark');
+            if (theme === 'dark') {
+                image.src = image.src.replace('Dark', 'Light');
+            } else {
+                image.src = image.src.replace('Light', 'Dark');
+            }
         });
+        document.body.classList.toggle('light_mode');
+        // Update theme in local storage
+        localStorage.setItem('theme', theme);
     }
 
+    // Apply initial theme
+    if (theme === 'light') {
+        toggleTheme();
+    }
+
+    // Event listener for theme toggle buttons
     themeToggleButtons.forEach(button => {
         button.addEventListener('click', () => {
-            if (theme === 'dark') {
-                theme = 'light';
-                document.body.classList.add('light_mode');
-                themeImages.forEach(image => {
-                    image.src = image.src.replace('Light', 'Dark');
-                    
-                });
-            } else {
-                theme = 'dark';
-                document.body.classList.remove('light_mode');
-                themeImages.forEach(image => {
-                    image.src = image.src.replace('Dark', 'Light');
-                });
-            }
-            localStorage.setItem('theme', theme);
+            theme = theme === 'dark' ? 'light' : 'dark';
+            toggleTheme();
         });
     });
 }
-
